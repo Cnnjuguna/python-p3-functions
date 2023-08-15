bb5ea30169f83479e68671fa2ee1ea8468e18a87
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-08-15 09:06:35 +0300

    Completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python3
 
+
 def greet_programmer():
     pass
+    print("Hello, programmer!")
 
-def greet(name):
+
+greet_programmer()
+
+
+def greet(name="Engineer"):
     pass
+    print(f"Hello, {name}!")
+
+
+greet()
+
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
 
-def add(num1, num2):
-    pass
 
-def halve(number):
+greet_with_default()
+
+
+def add(num1=45, num2=55):
     pass
+    return num1 + num2
+
+
+add()
+
+
+def halve(number=6.0):
+    pass
+    return number / 2
+
+
+halve()
